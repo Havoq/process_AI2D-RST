bf0810abd704e0d2134b97302ca4576cdf2782ef
thiippal <tuomo.hiippala@iki.fi>
2021-08-03 12:12:45 +0300

    add zscore example and argparser

--- process_labels/zscore.py
+++ process_labels/zscore.py
@@ -1,26 +1,41 @@
 # Import libraries
+import argparse
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import zscore
 
+# Set up the argument parser
+ap = argparse.ArgumentParser()
+
+# Define arguments
+ap.add_argument("-df", "--dataframe", required=True,
+                help="Path to the pickled DataFrame.")
+
+# Parse arguments
+args = vars(ap.parse_args())
+
+# Assign arguments to variables
+df_path = args['dataframe']
+
 # Configure matplotlib figure size and layout
 plt.figure(figsize=(16, 12))
 plt.tight_layout()
 
 # Read pickled DataFrame
-df = pd.read_pickle("processed_data/label_dataframe.pickle")
+df = pd.read_pickle(df_path)
 
 # Cross-tabulate counts for macro-groups and RST relation types
-crosstab = pd.crosstab(df['macro_group'], df['relation_type'], normalize='columns')
+crosstab = pd.crosstab(df['macro_group'], df['relation_type'])
 
 # Apply zscore() function from scipy to the cross-tabulated data;
 # round the floats to two decimals.
-# crosstab = crosstab.apply(zscore).round(2)
+crosstab = crosstab.apply(zscore).round(2)
 
 # Draw heatmap using seaborn
 heatmap = sns.heatmap(crosstab, annot=True, annot_kws={"size": 10}, cmap='viridis')
 
 # Save the heatmap to disk
 plt.savefig('zscore_heatmap.pdf', bbox_inches='tight', dpi=300)
+
