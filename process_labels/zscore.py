979136d7b3a3a19d75a9931beba9c4883515c980
thiippal <tuomo.hiippala@iki.fi>
2021-09-03 15:46:57 +0300

    filtering rare cases

--- process_labels/zscore.py
+++ process_labels/zscore.py
@@ -26,12 +26,20 @@ plt.tight_layout()
 # Read pickled DataFrame
 df = pd.read_pickle(df_path)
 
+# Count relation types and macro groups
+rel_counts = df['relation_type'].value_counts()
+macro_counts = df['macro_group'].value_counts()
+
+# Filter out relations and macro-groups with less than 30 observations
+df = df[~df['relation_type'].isin(rel_counts[rel_counts < 30].index)]
+df = df[~df['macro_group'].isin(macro_counts[macro_counts < 30].index)]
+
 # Cross-tabulate counts for macro-groups and RST relation types
-crosstab = pd.crosstab(df['macro_group'], df['relation_type'])
+crosstab = pd.crosstab(df['relation_type'], df['macro_group'])
 
 # Apply zscore() function from scipy to the cross-tabulated data;
 # round the floats to two decimals.
-crosstab = crosstab.apply(zscore).round(2)
+crosstab = crosstab.apply(zscore, axis=1).round(2)
 
 # Draw heatmap using seaborn
 heatmap = sns.heatmap(crosstab, annot=True, annot_kws={"size": 10}, cmap='viridis')
